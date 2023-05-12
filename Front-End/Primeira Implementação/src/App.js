6fba1e0921504b82fac63dae02e9741a4f46ee3f
Matheus-FSantos <matheus.fs.contato@gmail.com>
2023-05-11 22:34:15 -0300

    Upload Front-End primeira versão - Tela de cadastro

---
+++ Front-End/Primeira Implementação/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+/* CSS Archives */
+import './App.css';
+
+/* Components */
+import Form from './components/Form/Form';
+
+
+function App() {
+  return (
+    <div>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
